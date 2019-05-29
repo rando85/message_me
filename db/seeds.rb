2cca4b88cf39581734eaa92fac7d6f2af8826963
Daniel Cisneros <webmasterofpuppetz@gmail.com>
2019-05-29 00:57:23 -0500

    muchas cosas xD lol, hasta el partial de messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "snow", password: "123")
+User.create(username: "dany", password: "123")
+User.create(username: "arya", password: "123")
+User.create(username: "sansa", password: "123")
+User.create(username: "roto", password: "123")
